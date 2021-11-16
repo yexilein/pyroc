05bacb18f1781fd25d1366fb30e897641fc29de0
yexilein <stephischer@gmail.com>
2021-11-16 09:21:17 -0500

    Extend module interface

--- pyroc/__init__.py
+++ pyroc/__init__.py
@@ -1,3 +1,3 @@
 
-from .neighbor_voting import compute_neighbor_voting
-from .roc import Roc
+from .neighbor_voting import compute_neighbor_voting, compute_node_degree_performance, compute_prior_performance
+from .roc import Roc, compute_roc, compute_auroc, standardize_roc
