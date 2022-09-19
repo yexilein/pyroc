92e019e60f6f942c8c79073ece2de3ac701355da
Stephan  FISCHER <sfischer@WL22-0109.corp.pasteur.fr>
2022-09-19 11:23:02 +0200

    Add polygenic vs omnigenic plot

--- 02_markers/02_plot_fecs.R
+++ 02_markers/02_plot_fecs.R
@@ -31,7 +31,7 @@ make_fecs = function() {
 
     my_tissue = "Lung"
     lung_fec = select_fec(markers, my_cell_type, my_tissue, 0.05)
-    length(lung_fec$gene)
+    length(unique(lung_fec$gene))
     sum(fat20_panel %in% lung_fec$gene)
     lung_rocs = compute_all_rocs(markers, my_cell_type, unique(lung_fec$gene))
     write_csv(lung_fec, "fec/lung_fec.txt")
@@ -40,7 +40,7 @@ make_fecs = function() {
     
     my_tissue = "Spleen"
     spleen_fec = select_fec(markers, my_cell_type, my_tissue, 0.01)
-    length(spleen_fec$gene)
+    length(unique(spleen_fec$gene))
     spleen_rocs = compute_all_rocs(markers, my_cell_type, unique(spleen_fec$gene))
     write_csv(spleen_fec, "fec/spleen_fec.txt")
     write_csv(spleen_rocs$roc, "fec/spleen_roc.csv")
@@ -52,6 +52,14 @@ make_fecs = function() {
     write_csv(fat_fec, "fec/fat_fec.txt")
     write_csv(fat_rocs$roc, "fec/fat_roc.csv")
     write_csv(fat_rocs$auroc, "fec/fat_auroc.csv")
+    
+    # omnigenic vs polygenic?
+    # what happens when we pool FECs?
+    pooled_markers = unique(c(fat20_panel, spleen_fec$gene, lung_fec$gene))
+    length(pooled_markers)
+    pooled_fec_rocs = compute_all_rocs(markers, my_cell_type, pooled_markers)
+    write_csv(pooled_fec_rocs$roc, "fec/pooled_markers_roc.csv")
+    write_csv(pooled_fec_rocs$auroc, "fec/pooled_markers_auroc.csv")
 }
 
 select_initial_panel = function(markers, my_cell_type, my_train_tissue, my_train_mouse, top_n) {
@@ -211,6 +219,71 @@ plot_panels = function() {
     group_by(fat, is_10x) %>% summarize(auroc=mean(auroc)) %>% with(., var(auroc) / total_var)
     group_by(fat, mouse_id) %>% summarize(auroc=mean(auroc)) %>% with(., var(auroc) / total_var)
     group_by(fat, tissue) %>% summarize(auroc=mean(auroc))
+    
+    # omnigenic vs polygenic
+    rocs = read_csv("fec/spleen_roc.csv")
+    n_markers = 50
+    # what happens when we pool individuals?
+    average_rocs = rocs %>%
+        group_by(tissue) %>%
+        group_split() %>%
+        map(compute_average_roc) %>%
+        bind_rows() %>%
+        mutate(mouse_id = paste0("average_", tissue)) %>%
+        filter(n_curves > 2)
+    fecs = average_rocs %>%
+        mutate(n_positives = n_markers * n_curves) %>%
+        group_by(tissue, mouse_id) %>%
+        summarize(find_fecs(fpr, tpr, n_positives[1], 1), .groups="drop")
+    p1 = average_rocs %>%
+        plot_rocs(summary_only = TRUE) +
+        geom_segment(data = filter(fecs, fpr_end-fpr_start>0.05),
+                     aes(x=fpr_start,xend=fpr_end,y=tpr_start,yend=tpr_end), col="gray0") +
+        scale_color_manual(values = tissue_cols) +
+        facet_wrap(~ tissue)
+    ggsave("figs/pooled_individuals.pdf", p1)
+    p2 = p1 + coord_cartesian(xlim = c(0, 0.1)) + guides(col = "none")
+    ggsave("figs/pooled_individuals_zoom.pdf", p2)
+
+    # what happens when we pool tissues?
+    rocs %>%
+        select(mouse_id, tissue) %>%
+        distinct() %>%
+        group_by(mouse_id) %>%
+        filter(n()>2) %>%
+        nest()
+    average_rocs = rocs %>%
+        group_by(mouse_id) %>%
+        group_split() %>%
+        map(compute_average_roc, average_over = "tissue", group_var="mouse_id") %>%
+        bind_rows() %>%
+        mutate(tissue = mouse_id) %>%
+        filter(n_curves == 3)
+    fecs = average_rocs %>%
+        mutate(n_positives = n_markers * n_curves) %>%
+        group_by(tissue, mouse_id) %>%
+        summarize(find_fecs(fpr, tpr, n_positives[1], 1), .groups="drop")
+    p1 = average_rocs %>%
+        plot_rocs(summary_only = TRUE) +
+        geom_segment(data = mutate(filter(fecs, fpr_end-fpr_start>0.05), tissue = mouse_id),
+                     aes(x=fpr_start,xend=fpr_end,y=tpr_start,yend=tpr_end), col="gray0") +
+        facet_wrap(~ tissue)
+    ggsave("figs/pooled_tissues.pdf", p1)
+    p2 = p1 + coord_cartesian(xlim = c(0, 0.1)) + guides(col = "none")
+    ggsave("figs/pooled_tissues_zoom.pdf", p2)
+
+    global_average = mutate(rocs, mouse_tissue=paste(mouse_id, tissue), tissue="all") %>%
+        compute_average_roc(average_over = "mouse_tissue", group_var = "tissue") %>%
+        mutate(mouse_id = "all")
+    fecs = global_average %>%
+        mutate(n_positives = n_markers * n_curves) %>%
+        group_by(tissue, mouse_id) %>%
+        summarize(find_fecs(fpr, tpr, n_positives[1], 1), .groups="drop")
+    global_average %>%
+        plot_rocs(summary_only = TRUE) +
+        geom_segment(data = mutate(filter(fecs, fpr_end-fpr_start>0.05), tissue = mouse_id),
+                     aes(x=fpr_start,xend=fpr_end,y=tpr_start,yend=tpr_end), col="gray0")
+    ggsave("figs/global_average.pdf")
 }
 
 make_tissue_cols = function(tissues) {
@@ -238,18 +311,19 @@ plot_rocs = function(rocs, summary_only=FALSE) {
         labs(x="False Positive Rate", y="True Positive Rate", col=NULL)
 }
 
-compute_average_roc = function(roc_data) {
-    n_mice = length(unique(roc_data$mouse_id))
+compute_average_roc = function(roc_data, average_over = "mouse_id", group_var = "tissue") {
+    n_curves = length(unique(roc_data[[average_over]]))
     result = roc_data %>%
-        select(mouse_id, tpr, fpr) %>%
-        group_by(mouse_id, fpr) %>%
+        select(.data[[average_over]], tpr, fpr) %>%
+        group_by(.data[[average_over]], fpr) %>%
         summarize(min_tpr = min(tpr), max_tpr = max(tpr)) %>%
         group_by(fpr) %>%
-        filter(n() == n_mice) %>%
+        filter(n() == n_curves) %>%
         summarize(min_tpr = mean(min_tpr), max_tpr = mean(max_tpr)) %>%
         pivot_longer(-fpr, names_to = NULL, values_to = "tpr") %>%
         distinct()
-    result$tissue = roc_data$tissue[1]
+    result[[group_var]] = roc_data[[group_var]][1]
+    result$n_curves = n_curves
     return(result)
 }
 
@@ -262,8 +336,7 @@ plot_detailed_rocs = function(rocs) {
         facet_wrap(~ tissue)
 }
 
-compute_ks = function(fpr, tpr) {
-    n_positives = length(unique(tpr))-1
+compute_ks = function(fpr, tpr, n_positives = length(unique(tpr))-1) {
     delta_x = (fpr - min(fpr)) / (max(fpr)-min(fpr))
     delta_y = (tpr - min(tpr)) / (max(tpr)-min(tpr))
     local_deviation = abs(delta_y - delta_x)
@@ -286,16 +359,58 @@ ks_pval_approx = function(ks, n) {
 }
 
 pkstwo <- function(x, tol = 1e-06) {
-            if (is.numeric(x)) 
-                x <- as.double(x)
-            else stop("argument 'x' must be numeric")
-            p <- rep(0, length(x))
-            p[is.na(x)] <- NA
-            IND <- which(!is.na(x) & (x > 0))
-            if (length(IND)) 
-                p[IND] <- .Call(stats:::C_pKS2, p = x[IND], tol)
-            p
+    if (is.numeric(x)) 
+        x <- as.double(x)
+    else stop("argument 'x' must be numeric")
+    p <- rep(0, length(x))
+    p[is.na(x)] <- NA
+    IND <- which(!is.na(x) & (x > 0))
+    if (length(IND)) 
+        p[IND] <- .Call(stats:::C_pKS2, p = x[IND], tol)
+    p
+}
+
+find_fecs = function(fpr, tpr, n_positives, max_ks = 1) {
+    ks = compute_all_ks(fpr, tpr, n_positives)
+    find_linear_segments(fpr, tpr, ks, max_ks)
+}
+
+compute_all_ks = function(fpr, tpr, n_positives, min_genes=20) {
+    N = length(fpr)
+    result = matrix(NA, N, N)
+    delta_x = t(outer(fpr, fpr, "-"))
+    delta_y = t(outer(tpr, tpr, "-"))
+    for (start in seq(1, N-min_genes+1)) {
+        for (end in seq(start+min_genes-1, N)) {
+            scaled_x = delta_x[start, start:end] / delta_x[start, end]
+            scaled_y = delta_y[start, start:end] / delta_y[start, end]
+            local_deviation = abs(scaled_y - scaled_x)
+            local_deviation[is.na(local_deviation)] = 0
+            ks = max(local_deviation, na.rm = TRUE)
+            result[start, end] = ks * sqrt(n_positives*delta_y[start, end])
         }
+    }
+    return(result)
+}
+
+find_linear_segments = function(fpr, tpr, ks, max_ks) {
+    valid_segments = ks <= max_ks
+    valid_segments[is.na(valid_segments)] = FALSE
+    segment_length = t(outer(fpr, fpr, "-"))
+    segment_length[!valid_segments] = 0
+    segments = c()
+    while (max(segment_length) > 0) {
+        start_end = arrayInd(which.max(segment_length), dim(segment_length))
+        segments = rbind(segments, start_end)
+        segment_length[,start_end[1]:start_end[2]] = 0
+        segment_length[start_end[1]:start_end[2],] = 0
+    }
+    colnames(segments) = c("start", "end")
+    result = as_tibble(segments) %>%
+        rowwise() %>%
+        mutate(ks = ks[start, end], fpr_start = fpr[start], fpr_end = fpr[end], tpr_start = tpr[start], tpr_end = tpr[end])
+    return(result)
+}
 
 if (sys.nframe() == 0) {
     main()
